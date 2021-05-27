e1a44d9b1dc17731cdea12307241f14aeb891fce
Kr9091 <kr9091@gmail.com>
2021-05-26 22:46:03 -0700

    inital commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bf690b29e5089cf62ef49f3407aa1865'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e1506b79-f82d-4015-be74-280856219f72'
+};
